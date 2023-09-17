9aab37a9ba704fc63517f896dacc7fcfa2093489
Peter Davies <pgd@digitalacorn.co.uk>
2023-09-17 17:55:02 +0100

    feat: another preset

--- src/data/presets.js
+++ src/data/presets.js
@@ -8,6 +8,22 @@ export const presets = [
     },
   },
   {
+    name: 'Airport->Gosforth',
+    data: {
+      start: 27,
+      finish: 45,
+      clusters: [
+        [19, 39],
+        [2, 18, 28, 43, 1, 4, 20],
+        [17, 31, 36, 48, 37, 24, 6, 13, 41, 34],
+        [44, 15, 38, 3, 9, 29, 23],
+        [5, 21],
+        [47, 30, 16],
+      ],
+    },
+  },
+
+  {
     name: 'Airport->Newburn',
     data: {
       start: 27,
